657e4578186d14517c2545c42d43e30cfe53a585
PaulPEHARPRE <paul.peharpre@koji.fr>
2022-09-29 08:36:37 +0200

    Second Commit : Adding basic bricks of security

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from models import User
+from authentication_methods import get_current_user
 import uvicorn
 
 app = FastAPI()
-
 
 @app.get("/api/")
 def root():
     return "server up and running..."
 
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
-# reminder not to use reload in production
+# reminder not to use reload in production (too many rerenders)
